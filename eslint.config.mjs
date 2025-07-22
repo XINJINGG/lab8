fbe4009524db29c8246b6a734da94c2e0356d2d0
XINJINGG <xinjing-02@outlook.com>
2025-07-22 18:22:14 +0800

    edit config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,30 @@
-// import js from "@eslint/js";
-// import globals from "globals";
-// import { defineConfig } from "eslint/config";
-
-
-// export default defineConfig([
-//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-//   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-// ]);
-
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "semi": ["error", "always"],
-      "eqeqeq": "error",
-      "no-console": "warn",
-      "no-var": "error",
-    },
-    extends: ["eslint:recommended"],
-  },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
 ]);
+
+// import js from "@eslint/js";
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   {
+//     files: ["**/*.{js,mjs,cjs}"],
+//     ...js.configs.recommended, // brings in "rules", "languageOptions", etc.
+//     languageOptions: {
+//       globals: { ...globals.browser, ...globals.node },
+//     },
+//     rules: {
+//       "no-unused-vars": "warn",
+//       "semi": ["error", "always"],
+//       "eqeqeq": "error",
+//       "no-console": "warn",
+//       "no-var": "error",
+//     },
+//   },
+// ]);
